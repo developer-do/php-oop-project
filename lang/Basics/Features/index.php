57bfcec6709c6dc8d5a9ec3e8c45ffa21481cbf4
developer-do <dev-do@naver.com>
2020-06-17 23:40:57 +0900

    간단한 상속 정의 및 사용법

--- lang/Basics/Features/index.php
+++ lang/Basics/Features/index.php
@@ -6,13 +6,13 @@ class A
 {
     public $message = 'Hello, world'; // properties
 
-    public function foo() {
+    public function foo() { // method
         return $this->message;
     }
 }
 
 $a = new A();
-var_dump($a->foo());
+// var_dump($a->foo());
 
 
 
@@ -20,11 +20,35 @@ var_dump($a->foo());
  * Inherit
  */
 
+class B extends A 
+{
+
+}
+
+$b = new B();
+
+// var_dump($b->foo());
+
+
 /**
  * in Functions
  */
+function foo(A $a)
+{
+    return $a->foo();
+}
+
+var_dump(foo($b));
+
 
 /**
  * Context
+ */
+
+
+
+ 
+/**
+ * Constants
  */
 ?>
