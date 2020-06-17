71246f5b345283ef9b164aac83b212741321c105
developer-do <dev-do@naver.com>
2020-06-17 23:32:24 +0900

    클래스 안에 프로퍼티와 메서드 정의 방법 및 사용방법

---
+++ lang/Basics/Features/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Properties and Methods
+ */
+class A 
+{
+    public $message = 'Hello, world'; // properties
+
+    public function foo() {
+        return $this->message;
+    }
+}
+
+$a = new A();
+var_dump($a->foo());
+
+
+
+/**
+ * Inherit
+ */
+
+/**
+ * in Functions
+ */
+
+/**
+ * Context
+ */
+?>
